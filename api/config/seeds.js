6c031489190c618b417b9bd5a2a762cccc36239d
daniellelara <daniellelaragourgey@gmail.com>
2016-03-13 13:58:03 +0000

    seeds working

--- api/config/seeds.js
+++ api/config/seeds.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var User = require('../models/user');
-var Refugee = require('../models/Refugee');
+var Refugee = require('../models/refugee');
+
 mongoose.connect('mongodb://localhost:27017/refugee-app');
 User.collection.drop();
 Refugee.collection.drop();
@@ -35,7 +36,7 @@ User.create([{
   name: "Aamir",
   avatar: "http://i.telegraph.co.uk/multimedia/archive/02374/syrianRefegees2_2374507b.jpg",
   images: "http://i.telegraph.co.uk/multimedia/archive/03439/israel-banner_3439427b.jpg",
-  story: "'In our home in Damascus, I lived with my wife, our children, my parents, siblings and their family,' begins Aamir, a 29-year-old, Syrian that arrived in London nearly two years ago with his wife and two daughters.'Before Ramadan 2012, the conflict in Syria became markedly worse. We heard gunfire and no one would go out onto the streets. My family and I stayed in our house for days because we were afraid. I cannot tell you the exact number of days. On the last day, we had no bread. We had nothing to eat.'After days boarded up in their house, Aamir went out to get milk and diapers for his daughters.  At a checkpoint, the Freedom Army stopped him and tried to recruit him. He refused, which meant certain death if he were to be stopped again in the future. 'The last words from them were: the next time we see you, either you take your gun and stand beside us or you find someone to take your body.'"
+  story: "'In our home in Damascus, I lived with my wife, our children, my parents, siblings and their family,' begins Aamir, a 29-year-old, Syrian that arrived in London nearly two years ago with his wife and two daughters.'Before Ramadan 2012, the conflict in Syria became markedly worse. We heard gunfire and no one would go out onto the streets. My family and I stayed in our house for days because we were afraid. I cannot tell you the exact number of days. On the last day, we had no bread. We had nothing to eat.'After days boarded up in their house, Aamir went out to get milk and diapers for his daughters.  At a checkpoint, the Freedom Army stopped him and tried to recruit him. He refused, which meant certain death if he were to be stopped again in the future. 'The last words from them were: the next time we see you, either you take your gun and stand beside us or you find someone to take your body.'",
   lat: "35.091515",
   lng: "39.022323",
   location: "Syria",
@@ -45,7 +46,7 @@ User.create([{
       name: "Mitra",
       avatar: "http://cbsnews1.cbsistatic.com/hub/i/r/2015/09/07/2a5e417b-f0ff-439b-bb84-61f3bb09124b/thumbnail/620x350/dc56584258241ebb07b70c3612a20f97/jawan.jpg",
       images: "http://media.worldbulletin.net/news/2015/09/06/2015-09-06t110354z-1910273600-lr2eb960uq7ja-rtrmadp-3-europe-migrants.JPG",
-      story: "Sixteen-year-old Mitra left his native Afghanistan after his father was killed. He shared his story from a detention center in Northern Greece in Spring 2013.'My father was working for the military,' says Mitra. 'Our house was in Kunduz.' Local Taliban elements warned his father to stop working for the state; when he refused, they killed him and threatened the rest of his family.'I was also in danger,' says Mitra. 'My cousin told me to leave Afghanistan. My five brothers and sisters moved with my mother at my grandfather’s house. I haven’t spoken with them lately.'"
+      story: "Sixteen-year-old Mitra left his native Afghanistan after his father was killed. He shared his story from a detention center in Northern Greece in Spring 2013.'My father was working for the military,' says Mitra. 'Our house was in Kunduz.' Local Taliban elements warned his father to stop working for the state; when he refused, they killed him and threatened the rest of his family.'I was also in danger,' says Mitra. 'My cousin told me to leave Afghanistan. My five brothers and sisters moved with my mother at my grandfather’s house. I haven’t spoken with them lately.'",
       lat: "36.731947",
       lng: "68.867581",
       location: "Afghanistan",
