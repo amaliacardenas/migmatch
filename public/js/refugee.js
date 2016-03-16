db71eb52ce321612cef00ae301c94d7278513ffa
amaliacardenas <amaliacardenas@gmail.com>
2016-03-16 13:47:40 +0000

    saving before sass cache issues

--- public/js/refugee.js
+++ public/js/refugee.js
@@ -35,6 +35,7 @@ function displayRefugees(data) {
             "<div class='overlay'><h3>"+ data.name +"</h3>" +
             "<h4>"+ data.location +"</h4></div>" +
             "<p class='text' id='refugee-story-text'>"+ data.story + "</p>" +
+            "<p class='text' id='refugee-story-text'>" data.amountRaised + "</p>" +
             "<p><button class='more button btn btn-default' id="+ data._id + "> read more" + "</button></p></div></div></div>");
     $('.more').on('click', getOneRefugee);
     $show.append($li);
